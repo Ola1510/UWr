01d60093d10f56cfc3dc00ac5cd3194834a06e47
Ola1510 <st.aleksandra2000@gmial.com>
2022-02-10 23:43:44 +0100

    finished Python game - Wordle

---
+++ Wordle/colors.py
@@ -0,0 +1,13 @@
+
+background_color = (224, 224, 224)
+fill_color = (192, 192, 192)
+keyboard_buttons_color = 'black'
+keyboard_color = (179, 16, 130)
+
+exacly_color = (209, 100, 149)
+mostly_color = (228, 160, 201)
+wrong_color = 'grey'
+
+button_color = 'black'
+button_clicked_color = 'green'
+button_frame_color = 'black'
